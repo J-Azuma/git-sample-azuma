94c6977d4dd2e6bb494d52e158ce2a4fd2244a04
J-azuma <azumanotdetail@gmail.com>
2020-06-16 11:47:23 +0900

    create setter and setter of Member domain

--- src/main/java/com/example/domain/Member.java
+++ src/main/java/com/example/domain/Member.java
@@ -3,4 +3,26 @@ package com.example.domain;
 public class Member {
 	private String name;
 	private Integer age;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [name=" + name + ", age=" + age + "]";
+	}
+
 }
